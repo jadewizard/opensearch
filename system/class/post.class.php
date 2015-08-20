2ae9623af739cb38ebb9ac568304f5760dffb484
jadewizard <buryakovdima@yandex.ru>
2015-08-20 16:34:33 +0300

    Добавлен новый класс для вывода контента конкретного поста

--- system/class/post.class.php
+++ system/class/post.class.php
@@ -66,4 +66,34 @@ class UserContent extends ProjectContent
 
 }
 
+/*
+Класс для вывода содержимого конкретного поста
+на сайте.
+*/
+class getContent extends ProjectContent
+{
+    
+    /*
+    Функция достаёт из базы 
+    пост который имеет ID 
+    взятый из аргумента функии
+    getProjectContent и формерует
+    массив, который в дальнешейм
+    выводится на странице
+    поста. Массив передаётся
+    шаблонизатору.
+    */
+	public function getProjectContent($id)
+	{
+		global $twig,$db;
+        
+        $this->data = $db->getAll("SELECT * FROM os_projects_content WHERE id=".$id."");
+
+        $twig->addGlobal('content_array', $this->data[0]);
+
+        //return $this->data[0];
+	}
+
+}
+
 ?>
