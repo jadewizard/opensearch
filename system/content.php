02e1d98aea8841115e856bbe037d4f4fb25da708
jadewizard <buryakovdima@yandex.ru>
2015-08-20 19:18:51 +0300

    обработка ошибки 404 если страницы нет

--- system/content.php
+++ system/content.php
@@ -6,9 +6,18 @@ if (isset($_GET['page']) && $_GET['page'] == 'project')
 {
 	if (isset($_GET['id']))
 	{
-		$id = auto_clean($_GET['id']);
-		$getContent->getProjectContent($id);
-		//print_r($postContent);
+		$id = auto_clean((int) $_GET['id']);
+		$check = sql_check($id);
+
+		if (count($check) > 0)
+		{
+			$getContent->getProjectContent($id);
+		    //print_r($postContent);
+			$twig->addGlobal('project_id', $id-1);
+
+		} else {
+			header('Location: index.php?page=404');
+		}
 	}
 }
 
