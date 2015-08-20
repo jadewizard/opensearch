52fb0b723ed7e482cf7f3a8c33c529404bbba337
jadewizard <buryakovdima@yandex.ru>
2015-08-20 19:20:55 +0300

    + функция проверки на существование страницы

--- system/functions.php
+++ system/functions.php
@@ -13,4 +13,13 @@ function get_url()
 {
 	return "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; ;
 }
+
+function sql_check($id)
+{
+	global $db;
+
+	$query = $db -> getAll("SELECT title FROM os_projects_content WHERE id=".$id."");
+
+	return $query;
+}
 ?>
