afcba1136112358c47fe6ef8bdf5576a3e09da1c
jadewizard <jadewizard@yandex.ru>
2015-09-28 20:02:19 +0300

    Добавление данных при клике в БД

--- system/user_request.php
+++ system/user_request.php
@@ -1,5 +1,19 @@
 <?php
-$response = $userContent->userRequestInProject();
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
-echo $response;
+session_start();
+
+require_once 'class/users.class.php';
+require_once 'class/safemysql.class.php';
+
+$db = new SafeMysql(array('user' => 'root', 'pass' => '211996', 'db' => 'db', 'charset' => 'utf8'));
+$user = new UserFunctions;
+
+if (isset($_POST['id']))
+{
+    $db->query("INSERT INTO os_user_request (project_id,user_id) VALUES ('$_POST[id]','$_SESSION[user_id]')");
+}
+
 ?>
